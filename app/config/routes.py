6ab54e9c4f761cbc1c05d397c807ce9d7e93389a
dicka_ismaji <dicka_ismaji@quick.com>
2020-03-06 13:46:42 +0700

    commit aja dulu

--- app/config/routes.py
+++ app/config/routes.py
@@ -1,7 +1,24 @@
 from app import app
 from app.controller import mainController
+from flask import request
 
 @app.route('/')
-@app.route('/index')
+def home():
+    return "Hello world"
+
+@app.route('/user')
 def index():
     return mainController.index()
+
+@app.route('/user/<id>')
+def userDetail(id):
+    print(id)
+    return mainController.show(id)
+
+@app.route('/user', methods=['POST', 'GET'])
+def users():
+    if request.method == 'GET':
+        return mainController.index()
+    else:
+        return mainController.store()
+
--- app/controller/mainController.py
+++ app/controller/mainController.py
@@ -1,22 +1,55 @@
 from app.model.user import Users
 from app.config import response
+from flask import request
 
 def index():
     try:
         users = Users.query.all()
-        data = transform(users)
-
+        data = _transform(users)
         return response.ok(data, "success")
     except Exception as e:
-        return response.badRequest(null, 'failed')
         print(e)
+        return response.badRequest([], 'failed')
 
-def transform(obj):
+def show(id):
+    try:
+        users = Users.query.filter_by(id=id).first()
+        if not users:
+            return response.badRequest([], 'Empty..')
+        data = _singleTransform(users)
+        return response.ok(data, "success")
+    except Exception as e:
+        print(e)
+        return response.badRequest([], 'failed')
+
+def _transform(obj):
     array = []
     for i in obj:
-        array.append({
-            'id': i.id,
-            'name': i.name,
-            'email': i.email
-        })
+        array.append(_singleTransform(i))
     return array
+
+def _singleTransform(obj):
+    return {
+        'id': obj.id,
+        'name': obj.name,
+        'email': obj.email
+    }
+
+def store():
+    try:
+        print(request)
+        name = request.json['name']
+        email = request.json['email']
+        password = request.json['password']
+
+        users = Users(name=name, email=email)
+        users.setPassword(password)
+        db.session.add(users)
+        db.session.commit()
+
+        return response.ok([], 'Successfully create data')
+
+    except Exception as e:
+        print(e)
+        print("Ini error")
+        return response.ok([], 'Successfully create data')
